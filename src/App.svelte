e832c0e36a7b01e09d8f13b8ef1017e0bdb15d82
SynCROSS <51585533+yuto3125@users.noreply.github.com>
2021-01-14 22:40:27 +0900

    Event Modifiers

--- src/App.svelte
+++ src/App.svelte
@@ -19,3 +19,26 @@
 <button on:click|once={handleClick}
   >Don't touch this code and Click me more than Twice if you can</button
 >
+<p>The full list of modifiers:</p>
+<ul>
+  <li>
+    preventDefault — calls event.preventDefault() before running the handler.
+    Useful for client-side form handling, for example.
+  </li>
+  <li>
+    stopPropagation — calls event.stopPropagation(), preventing the event
+    reaching the next element
+  </li>
+  <li>
+    passive — improves scrolling performance on touch/wheel events (Svelte will
+    add it automatically where it's safe to do so)
+  </li>
+  <li>nonpassive — explicitly set passive: false</li>
+  <li>
+    capture — fires the handler during the capture phase instead of the bubbling
+    phase ()
+  </li>
+  <li>once — remove the handler after the first time it runs</li>
+  <li>self — only trigger handler if event.target is the element itself</li>
+</ul>
+You can chain modifiers together, e.g. on:click|once|capture=&lbrace;...&rbrace;
