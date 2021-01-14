4121897f217b892f4f7d2b344f96ee2a52ba4a3a
SynCROSS <51585533+yuto3125@users.noreply.github.com>
2021-01-14 21:35:57 +0900

    Statements

--- src/App.svelte
+++ src/App.svelte
@@ -5,12 +5,10 @@
     count += 1;
   }
 
-  $: doubled = count * 2;
+  $: console.log(count);
 </script>
 
 <button on:click={handleClick}>
   Clicked {count}
   {count === 1 ? 'time' : 'times'}
 </button>
-
-<p>{count} doubled is {doubled}</p>
