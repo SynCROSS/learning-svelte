2a4220ba60c59770899ca3420bbf543eb064ba3f
SynCROSS <51585533+yuto3125@users.noreply.github.com>
2021-01-15 23:02:03 +0900

    Dimensions

--- public/build/bundle.js
+++ public/build/bundle.js
@@ -56,11 +56,70 @@ var app = (function () {
         else if (node.getAttribute(attribute) !== value)
             node.setAttribute(attribute, value);
     }
+    function to_number(value) {
+        return value === '' ? null : +value;
+    }
     function children(element) {
         return Array.from(element.childNodes);
     }
+    function set_input_value(input, value) {
+        input.value = value == null ? '' : value;
+    }
     function set_style(node, key, value, important) {
         node.style.setProperty(key, value, important ? 'important' : '');
+    }
+    // unfortunately this can't be a constant as that wouldn't be tree-shakeable
+    // so we cache the result instead
+    let crossorigin;
+    function is_crossorigin() {
+        if (crossorigin === undefined) {
+            crossorigin = false;
+            try {
+                if (typeof window !== 'undefined' && window.parent) {
+                    void window.parent.document;
+                }
+            }
+            catch (error) {
+                crossorigin = true;
+            }
+        }
+        return crossorigin;
+    }
+    function add_resize_listener(node, fn) {
+        const computed_style = getComputedStyle(node);
+        if (computed_style.position === 'static') {
+            node.style.position = 'relative';
+        }
+        const iframe = element('iframe');
+        iframe.setAttribute('style', 'display: block; position: absolute; top: 0; left: 0; width: 100%; height: 100%; ' +
+            'overflow: hidden; border: 0; opacity: 0; pointer-events: none; z-index: -1;');
+        iframe.setAttribute('aria-hidden', 'true');
+        iframe.tabIndex = -1;
+        const crossorigin = is_crossorigin();
+        let unsubscribe;
+        if (crossorigin) {
+            iframe.src = "data:text/html,<script>onresize=function(){parent.postMessage(0,'*')}</script>";
+            unsubscribe = listen(window, 'message', (event) => {
+                if (event.source === iframe.contentWindow)
+                    fn();
+            });
+        }
+        else {
+            iframe.src = 'about:blank';
+            iframe.onload = () => {
+                unsubscribe = listen(iframe.contentWindow, 'resize', fn);
+            };
+        }
+        append(node, iframe);
+        return () => {
+            if (crossorigin) {
+                unsubscribe();
+            }
+            else if (unsubscribe && iframe.contentWindow) {
+                unsubscribe();
+            }
+            detach(iframe);
+        };
     }
     function custom_event(type, detail) {
         const e = document.createEvent('CustomEvent');
@@ -300,10 +359,6 @@ var app = (function () {
         else
             dispatch_dev('SvelteDOMSetAttribute', { node, attribute, value });
     }
-    function prop_dev(node, property, value) {
-        node[property] = value;
-        dispatch_dev('SvelteDOMSetProperty', { node, property, value });
-    }
     function set_data_dev(text, data) {
         data = '' + data;
         if (text.wholeText === data)
@@ -343,252 +398,114 @@ var app = (function () {
     const file = "src\\App.svelte";
 
     function create_fragment(ctx) {
-    	let h1;
+    	let input0;
+    	let t0;
+    	let input1;
     	let t1;
     	let p;
     	let t2;
-    	let a;
+    	let t3;
     	let t4;
     	let t5;
-    	let ul0;
-    	let li0;
+    	let t6;
     	let t7;
-    	let li1;
-    	let t9;
-    	let li2;
-    	let t11;
-    	let li3;
-    	let t13;
-    	let li4;
-    	let t15;
-    	let li5;
-    	let t17;
-    	let ul1;
-    	let li6;
-    	let t19;
-    	let li7;
-    	let t21;
-    	let li8;
-    	let t23;
-    	let li9;
-    	let t25;
-    	let li10;
-    	let t27;
-    	let div2;
-    	let video;
-    	let t28;
-    	let t29;
-    	let t30;
-    	let track;
-    	let video_src_value;
-    	let t31;
-    	let div1;
-    	let progress;
-    	let progress_value_value;
-    	let t32;
-    	let div0;
-    	let span0;
-    	let t33_value = format(/*time*/ ctx[0]) + "";
-    	let t33;
-    	let t34;
-    	let span1;
-    	let t38;
-    	let span2;
+    	let div;
+    	let span;
+    	let t8;
+    	let div_resize_listener;
     	let mounted;
     	let dispose;
 
     	const block = {
     		c: function create() {
-    			h1 = element("h1");
-    			h1.textContent = "Caminandes: Llamigos";
+    			input0 = element("input");
+    			t0 = space();
+    			input1 = element("input");
     			t1 = space();
     			p = element("p");
-    			t2 = text("From ");
-    			a = element("a");
-    			a.textContent = "Blender Open Projects";
-    			t4 = text(". CC-BY");
-    			t5 = text("\nThe complete set of bindings for {audio} and {video}\nis as follows — six readonly bindings...\n");
-    			ul0 = element("ul");
-    			li0 = element("li");
-    			li0.textContent = "duration (readonly) — the total duration of the video, in seconds";
+    			t2 = text("size: ");
+    			t3 = text(/*w*/ ctx[0]);
+    			t4 = text("px x ");
+    			t5 = text(/*h*/ ctx[1]);
+    			t6 = text("px");
     			t7 = space();
-    			li1 = element("li");
-    			li1.textContent = "buffered (readonly) — an array of {start, end} objects";
-    			t9 = space();
-    			li2 = element("li");
-    			li2.textContent = "seekable (readonly) — ditto";
-    			t11 = space();
-    			li3 = element("li");
-    			li3.textContent = "played (readonly) — ditto";
-    			t13 = space();
-    			li4 = element("li");
-    			li4.textContent = "seeking (readonly) — boolean";
-    			t15 = space();
-    			li5 = element("li");
-    			li5.textContent = "ended (readonly) — boolean";
-    			t17 = text("\n...and five two-way bindings:\n");
-    			ul1 = element("ul");
-    			li6 = element("li");
-    			li6.textContent = "currentTime — the current point in the video, in seconds";
-    			t19 = space();
-    			li7 = element("li");
-    			li7.textContent = "playbackRate — how fast to play the video, where 1 is 'normal'";
-    			t21 = space();
-    			li8 = element("li");
-    			li8.textContent = "paused — this one should be self-explanatory";
-    			t23 = space();
-    			li9 = element("li");
-    			li9.textContent = "volume — a value between 0 and 1";
-    			t25 = space();
-    			li10 = element("li");
-    			li10.textContent = "muted — a boolean value where true is muted";
-    			t27 = text("\nVideos additionally have readonly videoWidth and videoHeight bindings.\n");
-    			div2 = element("div");
-    			video = element("video");
-    			t28 = text("bind:currentTime=");
-    			t29 = text(/*time*/ ctx[0]);
-    			t30 = text("\n    bind:duration bind:paused\n    ");
-    			track = element("track");
-    			t31 = space();
-    			div1 = element("div");
-    			progress = element("progress");
-    			t32 = space();
-    			div0 = element("div");
-    			span0 = element("span");
-    			t33 = text(t33_value);
-    			t34 = space();
-    			span1 = element("span");
-    			span1.textContent = `click anywhere to ${/*paused*/ ctx[3] ? "play" : "pause"} / drag to seek`;
-    			t38 = space();
-    			span2 = element("span");
-    			span2.textContent = `${format(/*duration*/ ctx[2])}`;
-    			add_location(h1, file, 53, 0, 1375);
-    			attr_dev(a, "href", "https://cloud.blender.org/open-projects");
-    			add_location(a, file, 55, 7, 1416);
-    			add_location(p, file, 54, 0, 1405);
-    			add_location(li0, file, 62, 2, 1641);
-    			add_location(li1, file, 63, 2, 1718);
-    			add_location(li2, file, 64, 2, 1798);
-    			add_location(li3, file, 65, 2, 1837);
-    			add_location(li4, file, 66, 2, 1874);
-    			add_location(li5, file, 67, 2, 1914);
-    			add_location(ul0, file, 61, 0, 1634);
-    			add_location(li6, file, 71, 2, 1993);
-    			add_location(li7, file, 72, 2, 2061);
-    			add_location(li8, file, 73, 2, 2135);
-    			add_location(li9, file, 74, 2, 2191);
-    			add_location(li10, file, 75, 2, 2235);
-    			add_location(ul1, file, 70, 0, 1986);
-    			attr_dev(track, "kind", "captions");
-    			add_location(track, file, 86, 4, 2652);
-    			attr_dev(video, "poster", "https://sveltejs.github.io/assets/caminandes-llamigos.jpg");
-    			if (video.src !== (video_src_value = "https://sveltejs.github.io/assets/caminandes-llamigos.mp4")) attr_dev(video, "src", video_src_value);
-    			attr_dev(video, "class", "svelte-11bzk4c");
-    			add_location(video, file, 79, 2, 2373);
-    			progress.value = progress_value_value = /*time*/ ctx[0] / /*duration*/ ctx[2] || 0;
-    			attr_dev(progress, "class", "svelte-11bzk4c");
-    			add_location(progress, file, 90, 4, 2771);
-    			attr_dev(span0, "class", "time svelte-11bzk4c");
-    			add_location(span0, file, 93, 6, 2843);
-    			attr_dev(span1, "class", "svelte-11bzk4c");
-    			add_location(span1, file, 94, 6, 2890);
-    			attr_dev(span2, "class", "time svelte-11bzk4c");
-    			add_location(span2, file, 95, 6, 2970);
-    			attr_dev(div0, "class", "info svelte-11bzk4c");
-    			add_location(div0, file, 92, 4, 2818);
-    			attr_dev(div1, "class", "controls svelte-11bzk4c");
-    			set_style(div1, "opacity", /*duration*/ ctx[2] && /*showControls*/ ctx[1] ? 1 : 0);
-    			add_location(div1, file, 89, 2, 2692);
-    			attr_dev(div2, "class", "svelte-11bzk4c");
-    			add_location(div2, file, 78, 0, 2365);
+    			div = element("div");
+    			span = element("span");
+    			t8 = text(/*text*/ ctx[3]);
+    			attr_dev(input0, "type", "range");
+    			attr_dev(input0, "class", "svelte-d4seyy");
+    			add_location(input0, file, 7, 0, 72);
+    			attr_dev(input1, "placeholder", "Type Something");
+    			attr_dev(input1, "class", "svelte-d4seyy");
+    			add_location(input1, file, 8, 0, 113);
+    			add_location(p, file, 10, 0, 171);
+    			set_style(span, "font-size", /*size*/ ctx[2] + "px");
+    			attr_dev(span, "class", "svelte-d4seyy");
+    			add_location(span, file, 13, 2, 250);
+    			attr_dev(div, "class", "svelte-d4seyy");
+    			add_render_callback(() => /*div_elementresize_handler*/ ctx[6].call(div));
+    			add_location(div, file, 12, 0, 199);
     		},
     		l: function claim(nodes) {
     			throw new Error("options.hydrate only works if the component was compiled with the `hydratable: true` option");
     		},
     		m: function mount(target, anchor) {
-    			insert_dev(target, h1, anchor);
+    			insert_dev(target, input0, anchor);
+    			set_input_value(input0, /*size*/ ctx[2]);
+    			insert_dev(target, t0, anchor);
+    			insert_dev(target, input1, anchor);
+    			set_input_value(input1, /*text*/ ctx[3]);
     			insert_dev(target, t1, anchor);
     			insert_dev(target, p, anchor);
     			append_dev(p, t2);
-    			append_dev(p, a);
+    			append_dev(p, t3);
     			append_dev(p, t4);
-    			insert_dev(target, t5, anchor);
-    			insert_dev(target, ul0, anchor);
-    			append_dev(ul0, li0);
-    			append_dev(ul0, t7);
-    			append_dev(ul0, li1);
-    			append_dev(ul0, t9);
-    			append_dev(ul0, li2);
-    			append_dev(ul0, t11);
-    			append_dev(ul0, li3);
-    			append_dev(ul0, t13);
-    			append_dev(ul0, li4);
-    			append_dev(ul0, t15);
-    			append_dev(ul0, li5);
-    			insert_dev(target, t17, anchor);
-    			insert_dev(target, ul1, anchor);
-    			append_dev(ul1, li6);
-    			append_dev(ul1, t19);
-    			append_dev(ul1, li7);
-    			append_dev(ul1, t21);
-    			append_dev(ul1, li8);
-    			append_dev(ul1, t23);
-    			append_dev(ul1, li9);
-    			append_dev(ul1, t25);
-    			append_dev(ul1, li10);
-    			insert_dev(target, t27, anchor);
-    			insert_dev(target, div2, anchor);
-    			append_dev(div2, video);
-    			append_dev(video, t28);
-    			append_dev(video, t29);
-    			append_dev(video, t30);
-    			append_dev(video, track);
-    			append_dev(div2, t31);
-    			append_dev(div2, div1);
-    			append_dev(div1, progress);
-    			append_dev(div1, t32);
-    			append_dev(div1, div0);
-    			append_dev(div0, span0);
-    			append_dev(span0, t33);
-    			append_dev(div0, t34);
-    			append_dev(div0, span1);
-    			append_dev(div0, t38);
-    			append_dev(div0, span2);
+    			append_dev(p, t5);
+    			append_dev(p, t6);
+    			insert_dev(target, t7, anchor);
+    			insert_dev(target, div, anchor);
+    			append_dev(div, span);
+    			append_dev(span, t8);
+    			div_resize_listener = add_resize_listener(div, /*div_elementresize_handler*/ ctx[6].bind(div));
 
     			if (!mounted) {
     				dispose = [
-    					listen_dev(video, "mousemove", /*handleMousemove*/ ctx[4], false, false, false),
-    					listen_dev(video, "mousedown", /*handleMousedown*/ ctx[5], false, false, false)
+    					listen_dev(input0, "change", /*input0_change_input_handler*/ ctx[4]),
+    					listen_dev(input0, "input", /*input0_change_input_handler*/ ctx[4]),
+    					listen_dev(input1, "input", /*input1_input_handler*/ ctx[5])
     				];
 
     				mounted = true;
     			}
     		},
     		p: function update(ctx, [dirty]) {
-    			if (dirty & /*time*/ 1) set_data_dev(t29, /*time*/ ctx[0]);
-
-    			if (dirty & /*time*/ 1 && progress_value_value !== (progress_value_value = /*time*/ ctx[0] / /*duration*/ ctx[2] || 0)) {
-    				prop_dev(progress, "value", progress_value_value);
+    			if (dirty & /*size*/ 4) {
+    				set_input_value(input0, /*size*/ ctx[2]);
     			}
 
-    			if (dirty & /*time*/ 1 && t33_value !== (t33_value = format(/*time*/ ctx[0]) + "")) set_data_dev(t33, t33_value);
+    			if (dirty & /*text*/ 8 && input1.value !== /*text*/ ctx[3]) {
+    				set_input_value(input1, /*text*/ ctx[3]);
+    			}
 
-    			if (dirty & /*showControls*/ 2) {
-    				set_style(div1, "opacity", /*duration*/ ctx[2] && /*showControls*/ ctx[1] ? 1 : 0);
+    			if (dirty & /*w*/ 1) set_data_dev(t3, /*w*/ ctx[0]);
+    			if (dirty & /*h*/ 2) set_data_dev(t5, /*h*/ ctx[1]);
+    			if (dirty & /*text*/ 8) set_data_dev(t8, /*text*/ ctx[3]);
+
+    			if (dirty & /*size*/ 4) {
+    				set_style(span, "font-size", /*size*/ ctx[2] + "px");
     			}
     		},
     		i: noop,
     		o: noop,
     		d: function destroy(detaching) {
-    			if (detaching) detach_dev(h1);
+    			if (detaching) detach_dev(input0);
+    			if (detaching) detach_dev(t0);
+    			if (detaching) detach_dev(input1);
     			if (detaching) detach_dev(t1);
     			if (detaching) detach_dev(p);
-    			if (detaching) detach_dev(t5);
-    			if (detaching) detach_dev(ul0);
-    			if (detaching) detach_dev(t17);
-    			if (detaching) detach_dev(ul1);
-    			if (detaching) detach_dev(t27);
-    			if (detaching) detach_dev(div2);
+    			if (detaching) detach_dev(t7);
+    			if (detaching) detach_dev(div);
+    			div_resize_listener();
     			mounted = false;
     			run_all(dispose);
     		}
@@ -605,82 +522,58 @@ var app = (function () {
     	return block;
     }
 
-    function format(seconds) {
-    	if (isNaN(seconds)) return "...";
-    	const minutes = Math.floor(seconds / 60);
-    	seconds = Math.floor(seconds % 60);
-    	if (seconds < 10) seconds = "0" + seconds;
-    	return `${minutes}:${seconds}`;
-    }
-
     function instance($$self, $$props, $$invalidate) {
     	let { $$slots: slots = {}, $$scope } = $$props;
     	validate_slots("App", slots, []);
-    	let time = 0;
-    	let duration;
-    	let paused = true;
-    	let showControls = true;
-    	let showControlsTimeout;
-
-    	function handleMousemove(e) {
-    		// Make the controls visible, but fade out after
-    		// 2.5 seconds of inactivity
-    		clearTimeout(showControlsTimeout);
-
-    		showControlsTimeout = setTimeout(() => $$invalidate(1, showControls = false), 2500);
-    		$$invalidate(1, showControls = true);
-    		if (!(e.buttons & 1)) return; // mouse not down
-    		if (!duration) return; // video not loaded yet
-    		const { left, right } = this.getBoundingClientRect();
-    		$$invalidate(0, time = duration * (e.clientX - left) / (right - left));
-    	}
-
-    	function handleMousedown(e) {
-    		// we can't rely on the built-in click event, because it fires
-    		// after a drag — we have to listen for clicks ourselves
-    		function handleMouseup() {
-    			if (paused) e.target.play(); else e.target.pause();
-    			cancel();
-    		}
-
-    		function cancel() {
-    			e.target.removeEventListener("mouseup", handleMouseup);
-    		}
-
-    		e.target.addEventListener("mouseup", handleMouseup);
-    		setTimeout(cancel, 200);
-    	}
-
+    	let w;
+    	let h;
+    	let size = 42;
+    	let text = "";
     	const writable_props = [];
 
     	Object.keys($$props).forEach(key => {
     		if (!~writable_props.indexOf(key) && key.slice(0, 2) !== "$$") console.warn(`<App> was created with unknown prop '${key}'`);
     	});
 
-    	$$self.$capture_state = () => ({
-    		time,
-    		duration,
-    		paused,
-    		showControls,
-    		showControlsTimeout,
-    		handleMousemove,
-    		handleMousedown,
-    		format
-    	});
+    	function input0_change_input_handler() {
+    		size = to_number(this.value);
+    		$$invalidate(2, size);
+    	}
+
+    	function input1_input_handler() {
+    		text = this.value;
+    		$$invalidate(3, text);
+    	}
+
+    	function div_elementresize_handler() {
+    		w = this.clientWidth;
+    		h = this.clientHeight;
+    		$$invalidate(0, w);
+    		$$invalidate(1, h);
+    	}
+
+    	$$self.$capture_state = () => ({ w, h, size, text });
 
     	$$self.$inject_state = $$props => {
-    		if ("time" in $$props) $$invalidate(0, time = $$props.time);
-    		if ("duration" in $$props) $$invalidate(2, duration = $$props.duration);
-    		if ("paused" in $$props) $$invalidate(3, paused = $$props.paused);
-    		if ("showControls" in $$props) $$invalidate(1, showControls = $$props.showControls);
-    		if ("showControlsTimeout" in $$props) showControlsTimeout = $$props.showControlsTimeout;
+    		if ("w" in $$props) $$invalidate(0, w = $$props.w);
+    		if ("h" in $$props) $$invalidate(1, h = $$props.h);
+    		if ("size" in $$props) $$invalidate(2, size = $$props.size);
+    		if ("text" in $$props) $$invalidate(3, text = $$props.text);
     	};
 
     	if ($$props && "$$inject" in $$props) {
     		$$self.$inject_state($$props.$$inject);
     	}
 
-    	return [time, showControls, duration, paused, handleMousemove, handleMousedown];
+    	return [
+    		w,
+    		h,
+    		size,
+    		text,
+    		input0_change_input_handler,
+    		input1_input_handler,
+    		div_elementresize_handler
+    	];
     }
 
     class App extends SvelteComponentDev {
