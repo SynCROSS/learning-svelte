0913555613ff81d87d5da5789d44eb6cd462c10e
SynCROSS <51585533+yuto3125@users.noreply.github.com>
2021-01-17 14:26:37 +0900

    Custom JS Transitions

--- src/App.svelte
+++ src/App.svelte
@@ -1,22 +1,25 @@
 <script>
-  import { fade } from 'svelte/transition';
-  import { elasticOut } from 'svelte/easing';
+  let visible = false;
 
-  let visible = true;
+  function typewriter(node, { speed = 10 }) {
+    const valid =
+      node.childNodes.length === 1 &&
+      node.childNodes[0].nodeType === Node.TEXT_NODE;
 
-  function spin(node, { duration }) {
+    if (!valid) {
+      throw new Error(
+        `This transition only works on elements with a single text node child`,
+      );
+    }
+
+    const text = node.textContent;
+    const duration = text.length * speed;
+
     return {
       duration,
-      css: t => {
-        const eased = elasticOut(t);
-
-        return `
-					transform: scale(${eased}) rotate(${eased * 1080}deg);
-					color: hsl(
-						${~~(t * 360)},
-						${Math.min(100, 1000 - 1000 * t)}%,
-						${Math.min(50, 500 - 500 * t)}%
-          );`;
+      tick: t => {
+        const i = ~~(text.length * t);
+        node.textContent = text.slice(0, i);
       },
     };
   }
@@ -28,43 +31,5 @@
 </label>
 
 {#if visible}
-  <div class="centered" in:spin={{ duration: 8000 }} out:fade>
-    <span>transitions!</span>
-  </div>
+  <p in:typewriter>The quick silver firefox jumps over the lazy dog.</p>
 {/if}
-<br />
-The function takes two arguments — the node to which the transition is applied,
-<br />
-and any parameters that were passed in — and returns a transition object which can
-have the following properties:
-
-<ul>
-  <li>delay — milliseconds before the transition begins</li>
-  <li>duration — length of the transition in milliseconds</li>
-  <li>easing — a p =&gt; t easing function (see the chapter on )</li>
-  <li>css — a (t, u) =&gt; css function, where u === 1 - t</li>
-  <li>
-    tick — a (t, u) =&gt; &lbrace;...&rbrace; function that has some effect on
-    the node
-  </li>
-</ul>
-The t value is 0 at the beginning of an intro or the end of an outro, and 1 at the
-end of an intro or beginning of an outro.<br />Most of the time you should
-return the css property and not the tick property, as CSS animations run off the
-main thread to prevent jank where possible.<br />
-Svelte 'simulates' the transition and constructs a CSS animation, then lets it run.
-
-<style>
-  .centered {
-    position: absolute;
-    left: 50%;
-    top: 50%;
-    transform: translate(-50%, -50%);
-  }
-
-  span {
-    position: absolute;
-    transform: translate(-50%, -50%);
-    font-size: 4em;
-  }
-</style>
