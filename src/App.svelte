87fce60b6d89d411d7a2a5db4a8a79c1de5b87d9
SynCROSS <51585533+yuto3125@users.noreply.github.com>
2021-01-21 23:24:14 +0900

    setContext & getContext

--- src/App.svelte
+++ src/App.svelte
@@ -1,31 +1,28 @@
 <script>
-  import Hoverable from './Hoverable.svelte';
+  import Map from './Map.svelte';
+  import MapMarker from './MapMarker.svelte';
 </script>
 
-<Hoverable let:hovering={active}>
-  <div class:active>
-    {#if active}
-      <p>I am being hovered upon.</p>
-    {:else}
-      <p>Hover over me!</p>
-    {/if}
-  </div>
-</Hoverable>
-
 <p>
-  Named slots can also have props; use the let directive on an element with a
-  slot="..." attribute, instead of on the component itself.
+  Contexts and stores seem similar. They differ in that stores are available to
+  any part of an app, while a context is only available to a component and its
+  descendants. This can be helpful if you want to use several instances of a
+  component without the state of one interfering with the state of the others.
 </p>
 
-<style>
-  div {
-    padding: 1em;
-    margin: 0 0 1em 0;
-    background-color: #eee;
-  }
-
-  .active {
-    background-color: #dc143c;
-    color: white;
-  }
-</style>
+<Map lat={35} lon={-84} zoom={3.5}>
+  <MapMarker lat={37.8225} lon={-122.0024} label="Svelte Body Shaping" />
+  <MapMarker
+    lat={33.8981}
+    lon={-118.4169}
+    label="Svelte Barbershop & Essentials"
+  />
+  <MapMarker lat={29.723} lon={-95.4189} label="Svelte Waxing Studio" />
+  <MapMarker
+    lat={28.3378}
+    lon={-81.3966}
+    label="Svelte 30 Nutritional Consultants"
+  />
+  <MapMarker lat={40.6483} lon={-74.0237} label="Svelte Brands LLC" />
+  <MapMarker lat={40.6986} lon={-74.41} label="Svelte Medical Systems" />
+</Map>
