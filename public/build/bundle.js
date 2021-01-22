30b2afaadb3624df8c4485ea1067b2fbe7eb73e2
SynCROSS <51585533+yuto3125@users.noreply.github.com>
2021-01-22 21:36:09 +0900

    svelte:body

--- public/build/bundle.js
+++ public/build/bundle.js
@@ -27,21 +27,11 @@ var app = (function () {
     function is_empty(obj) {
         return Object.keys(obj).length === 0;
     }
-
-    function append(target, node) {
-        target.appendChild(node);
-    }
     function insert(target, node, anchor) {
         target.insertBefore(node, anchor || null);
     }
     function detach(node) {
         node.parentNode.removeChild(node);
-    }
-    function destroy_each(iterations, detaching) {
-        for (let i = 0; i < iterations.length; i += 1) {
-            if (iterations[i])
-                iterations[i].d(detaching);
-        }
     }
     function element(name) {
         return document.createElement(name);
@@ -65,8 +55,8 @@ var app = (function () {
     function children(element) {
         return Array.from(element.childNodes);
     }
-    function set_style(node, key, value, important) {
-        node.style.setProperty(key, value, important ? 'important' : '');
+    function toggle_class(element, name, toggle) {
+        element.classList[toggle ? 'add' : 'remove'](name);
     }
     function custom_event(type, detail) {
         const e = document.createEvent('CustomEvent');
@@ -274,10 +264,6 @@ var app = (function () {
     function dispatch_dev(type, detail) {
         document.dispatchEvent(custom_event(type, Object.assign({ version: '3.31.2' }, detail)));
     }
-    function append_dev(target, node) {
-        dispatch_dev('SvelteDOMInsert', { target, node });
-        append(target, node);
-    }
     function insert_dev(target, node, anchor) {
         dispatch_dev('SvelteDOMInsert', { target, node, anchor });
         insert(target, node, anchor);
@@ -305,22 +291,6 @@ var app = (function () {
             dispatch_dev('SvelteDOMRemoveAttribute', { node, attribute });
         else
             dispatch_dev('SvelteDOMSetAttribute', { node, attribute, value });
-    }
-    function set_data_dev(text, data) {
-        data = '' + data;
-        if (text.wholeText === data)
-            return;
-        dispatch_dev('SvelteDOMSetData', { node: text, data });
-        text.data = data;
-    }
-    function validate_each_argument(arg) {
-        if (typeof arg !== 'string' && !(arg && typeof arg === 'object' && 'length' in arg)) {
-            let msg = '{#each} only iterates over array-like objects.';
-            if (typeof Symbol === 'function' && arg && Symbol.iterator in arg) {
-                msg += ' You can use a spread to convert this iterable into an array.';
-            }
-            throw new Error(msg);
-        }
     }
     function validate_slots(name, slot, keys) {
         for (const slot_key of Object.keys(slot)) {
@@ -353,257 +323,61 @@ var app = (function () {
 
     const file = "src\\App.svelte";
 
-    function get_each_context(ctx, list, i) {
-    	const child_ctx = ctx.slice();
-    	child_ctx[3] = list[i];
-    	return child_ctx;
-    }
-
-    // (10:2) {#each layers as layer}
-    function create_each_block(ctx) {
+    function create_fragment(ctx) {
+    	let t0;
     	let img;
     	let img_src_value;
-
-    	const block = {
-    		c: function create() {
-    			img = element("img");
-    			set_style(img, "transform", "translate(0," + -/*y*/ ctx[0] * /*layer*/ ctx[3] / (/*layers*/ ctx[1].length - 1) + "px)");
-    			if (img.src !== (img_src_value = "https://www.firewatchgame.com/images/parallax/parallax" + /*layer*/ ctx[3] + ".png")) attr_dev(img, "src", img_src_value);
-    			attr_dev(img, "alt", "parallax layer " + /*layer*/ ctx[3]);
-    			attr_dev(img, "class", "svelte-185d0v3");
-    			add_location(img, file, 10, 4, 210);
-    		},
-    		m: function mount(target, anchor) {
-    			insert_dev(target, img, anchor);
-    		},
-    		p: function update(ctx, dirty) {
-    			if (dirty & /*y*/ 1) {
-    				set_style(img, "transform", "translate(0," + -/*y*/ ctx[0] * /*layer*/ ctx[3] / (/*layers*/ ctx[1].length - 1) + "px)");
-    			}
-    		},
-    		d: function destroy(detaching) {
-    			if (detaching) detach_dev(img);
-    		}
-    	};
-
-    	dispatch_dev("SvelteRegisterBlock", {
-    		block,
-    		id: create_each_block.name,
-    		type: "each",
-    		source: "(10:2) {#each layers as layer}",
-    		ctx
-    	});
-
-    	return block;
-    }
-
-    function create_fragment(ctx) {
-    	let scrolling = false;
-
-    	let clear_scrolling = () => {
-    		scrolling = false;
-    	};
-
-    	let scrolling_timeout;
-    	let a;
-    	let t0;
-    	let div2;
-    	let span;
     	let t1;
-    	let t2;
-    	let div0;
-    	let t3;
-    	let t4;
-    	let t5;
-    	let t6;
-    	let div1;
-    	let t7;
-    	let ul;
-    	let li0;
-    	let t9;
-    	let li1;
-    	let t11;
-    	let li2;
-    	let t13;
-    	let li3;
-    	let t15;
-    	let li4;
-    	let t17;
-    	let li5;
-    	let t19;
-    	let li6;
-    	let t21;
+    	let p;
     	let mounted;
     	let dispose;
-    	add_render_callback(/*onwindowscroll*/ ctx[2]);
-    	let each_value = /*layers*/ ctx[1];
-    	validate_each_argument(each_value);
-    	let each_blocks = [];
-
-    	for (let i = 0; i < each_value.length; i += 1) {
-    		each_blocks[i] = create_each_block(get_each_context(ctx, each_value, i));
-    	}
 
     	const block = {
     		c: function create() {
-    			a = element("a");
-
-    			for (let i = 0; i < each_blocks.length; i += 1) {
-    				each_blocks[i].c();
-    			}
-
     			t0 = space();
-    			div2 = element("div");
-    			span = element("span");
-    			t1 = text("scroll down");
-    			t2 = space();
-    			div0 = element("div");
-    			t3 = text("You have scrolled ");
-    			t4 = text(/*y*/ ctx[0]);
-    			t5 = text(" pixels");
-    			t6 = space();
-    			div1 = element("div");
-    			t7 = text("The list of properties you can bind to is as follows:\n\n    ");
-    			ul = element("ul");
-    			li0 = element("li");
-    			li0.textContent = "innerWidth";
-    			t9 = space();
-    			li1 = element("li");
-    			li1.textContent = "innerHeight";
-    			t11 = space();
-    			li2 = element("li");
-    			li2.textContent = "outerWidth";
-    			t13 = space();
-    			li3 = element("li");
-    			li3.textContent = "outerHeight";
-    			t15 = space();
-    			li4 = element("li");
-    			li4.textContent = "scrollX";
-    			t17 = space();
-    			li5 = element("li");
-    			li5.textContent = "scrollY";
-    			t19 = space();
-    			li6 = element("li");
-    			li6.textContent = "online — an alias for window.navigator.onLine";
-    			t21 = text("\n    All except scrollX and scrollY are readonly.");
-    			attr_dev(a, "class", "parallax-container svelte-185d0v3");
-    			attr_dev(a, "href", "https://www.firewatchgame.com");
-    			add_location(a, file, 8, 0, 112);
-    			set_style(span, "opacity", 1 - Math.max(0, /*y*/ ctx[0] / 40));
-    			attr_dev(span, "class", "svelte-185d0v3");
-    			add_location(span, file, 19, 2, 449);
-    			attr_dev(div0, "class", "foreground svelte-185d0v3");
-    			add_location(div0, file, 21, 2, 522);
-    			add_location(li0, file, 29, 6, 691);
-    			add_location(li1, file, 30, 6, 717);
-    			add_location(li2, file, 31, 6, 744);
-    			add_location(li3, file, 32, 6, 770);
-    			add_location(li4, file, 33, 6, 797);
-    			add_location(li5, file, 34, 6, 820);
-    			add_location(li6, file, 35, 6, 843);
-    			attr_dev(ul, "class", "svelte-185d0v3");
-    			add_location(ul, file, 28, 4, 680);
-    			attr_dev(div1, "class", "foreground svelte-185d0v3");
-    			add_location(div1, file, 25, 2, 592);
-    			attr_dev(div2, "class", "text svelte-185d0v3");
-    			add_location(div2, file, 18, 0, 428);
+    			img = element("img");
+    			t1 = space();
+    			p = element("p");
+    			p.textContent = "Similar to <svelte:window>, the <svelte:body> element allows you\n  to listen for events that fire on document.body. This is useful with the\n  mouseenter and mouseleave events, which don't fire on window.";
+    			attr_dev(img, "alt", "Kitten wants to know what's going on");
+    			if (img.src !== (img_src_value = "https://svelte.dev/tutorial/kitten.png")) attr_dev(img, "src", img_src_value);
+    			attr_dev(img, "class", "svelte-12yt33k");
+    			toggle_class(img, "curious", /*hereKitty*/ ctx[0]);
+    			add_location(img, file, 12, 0, 319);
+    			add_location(p, file, 18, 0, 448);
     		},
     		l: function claim(nodes) {
     			throw new Error("options.hydrate only works if the component was compiled with the `hydratable: true` option");
     		},
     		m: function mount(target, anchor) {
-    			insert_dev(target, a, anchor);
-
-    			for (let i = 0; i < each_blocks.length; i += 1) {
-    				each_blocks[i].m(a, null);
-    			}
-
     			insert_dev(target, t0, anchor);
-    			insert_dev(target, div2, anchor);
-    			append_dev(div2, span);
-    			append_dev(span, t1);
-    			append_dev(div2, t2);
-    			append_dev(div2, div0);
-    			append_dev(div0, t3);
-    			append_dev(div0, t4);
-    			append_dev(div0, t5);
-    			append_dev(div2, t6);
-    			append_dev(div2, div1);
-    			append_dev(div1, t7);
-    			append_dev(div1, ul);
-    			append_dev(ul, li0);
-    			append_dev(ul, t9);
-    			append_dev(ul, li1);
-    			append_dev(ul, t11);
-    			append_dev(ul, li2);
-    			append_dev(ul, t13);
-    			append_dev(ul, li3);
-    			append_dev(ul, t15);
-    			append_dev(ul, li4);
-    			append_dev(ul, t17);
-    			append_dev(ul, li5);
-    			append_dev(ul, t19);
-    			append_dev(ul, li6);
-    			append_dev(div1, t21);
+    			insert_dev(target, img, anchor);
+    			insert_dev(target, t1, anchor);
+    			insert_dev(target, p, anchor);
 
     			if (!mounted) {
-    				dispose = listen_dev(window, "scroll", () => {
-    					scrolling = true;
-    					clearTimeout(scrolling_timeout);
-    					scrolling_timeout = setTimeout(clear_scrolling, 100);
-    					/*onwindowscroll*/ ctx[2]();
-    				});
+    				dispose = [
+    					listen_dev(document.body, "mouseenter", /*handleMouseenter*/ ctx[1], false, false, false),
+    					listen_dev(document.body, "mouseleave", /*handleMouseleave*/ ctx[2], false, false, false)
+    				];
 
     				mounted = true;
     			}
     		},
     		p: function update(ctx, [dirty]) {
-    			if (dirty & /*y*/ 1 && !scrolling) {
-    				scrolling = true;
-    				clearTimeout(scrolling_timeout);
-    				scrollTo(window.pageXOffset, /*y*/ ctx[0]);
-    				scrolling_timeout = setTimeout(clear_scrolling, 100);
+    			if (dirty & /*hereKitty*/ 1) {
+    				toggle_class(img, "curious", /*hereKitty*/ ctx[0]);
     			}
-
-    			if (dirty & /*y, layers*/ 3) {
-    				each_value = /*layers*/ ctx[1];
-    				validate_each_argument(each_value);
-    				let i;
-
-    				for (i = 0; i < each_value.length; i += 1) {
-    					const child_ctx = get_each_context(ctx, each_value, i);
-
-    					if (each_blocks[i]) {
-    						each_blocks[i].p(child_ctx, dirty);
-    					} else {
-    						each_blocks[i] = create_each_block(child_ctx);
-    						each_blocks[i].c();
-    						each_blocks[i].m(a, null);
-    					}
-    				}
-
-    				for (; i < each_blocks.length; i += 1) {
-    					each_blocks[i].d(1);
-    				}
-
-    				each_blocks.length = each_value.length;
-    			}
-
-    			if (dirty & /*y*/ 1) {
-    				set_style(span, "opacity", 1 - Math.max(0, /*y*/ ctx[0] / 40));
-    			}
-
-    			if (dirty & /*y*/ 1) set_data_dev(t4, /*y*/ ctx[0]);
     		},
     		i: noop,
     		o: noop,
     		d: function destroy(detaching) {
-    			if (detaching) detach_dev(a);
-    			destroy_each(each_blocks, detaching);
     			if (detaching) detach_dev(t0);
-    			if (detaching) detach_dev(div2);
+    			if (detaching) detach_dev(img);
+    			if (detaching) detach_dev(t1);
+    			if (detaching) detach_dev(p);
     			mounted = false;
-    			dispose();
+    			run_all(dispose);
     		}
     	};
 
@@ -621,29 +395,30 @@ var app = (function () {
     function instance($$self, $$props, $$invalidate) {
     	let { $$slots: slots = {}, $$scope } = $$props;
     	validate_slots("App", slots, []);
-    	const layers = [0, 1, 2, 3, 4, 5, 6, 7, 8];
-    	let y;
+    	let hereKitty = false;
+    	const handleMouseenter = () => $$invalidate(0, hereKitty = true);
+    	const handleMouseleave = () => $$invalidate(0, hereKitty = false);
     	const writable_props = [];
 
     	Object.keys($$props).forEach(key => {
     		if (!~writable_props.indexOf(key) && key.slice(0, 2) !== "$$") console.warn(`<App> was created with unknown prop '${key}'`);
     	});
 
-    	function onwindowscroll() {
-    		$$invalidate(0, y = window.pageYOffset);
-    	}
-
-    	$$self.$capture_state = () => ({ layers, y });
+    	$$self.$capture_state = () => ({
+    		hereKitty,
+    		handleMouseenter,
+    		handleMouseleave
+    	});
 
     	$$self.$inject_state = $$props => {
-    		if ("y" in $$props) $$invalidate(0, y = $$props.y);
+    		if ("hereKitty" in $$props) $$invalidate(0, hereKitty = $$props.hereKitty);
     	};
 
     	if ($$props && "$$inject" in $$props) {
     		$$self.$inject_state($$props.$$inject);
     	}
 
-    	return [y, layers, onwindowscroll];
+    	return [hereKitty, handleMouseenter, handleMouseleave];
     }
 
     class App extends SvelteComponentDev {
