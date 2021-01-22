3ed05c7ebfe2aee790d59481e26812dd76fe678c
SynCROSS <51585533+yuto3125@users.noreply.github.com>
2021-01-22 23:10:54 +0900

    Module Context

--- src/App.svelte
+++ src/App.svelte
@@ -1,67 +1,56 @@
 <script>
-  import Todo from './Todo.svelte';
-
-  let todos = [
-    { id: 1, done: false, text: 'wash the car' },
-    { id: 2, done: false, text: 'take the dog for a walk' },
-    { id: 3, done: false, text: 'mow the lawn' },
-  ];
-
-  function toggle(toggled) {
-    todos = todos.map(todo => {
-      if (todo === toggled) {
-        // return a new object
-        return {
-          id: todo.id,
-          text: todo.text,
-          done: !todo.done,
-        };
-      }
-
-      // return the same object
-      return todo;
-    });
-  }
+  import AudioPlayer from './AudioPlayer.svelte';
 </script>
 
-<h2>Todos</h2>
-{#each todos as todo}
-  <Todo {todo} on:click={() => toggle(todo)} />
-{/each}
-
 <p>
-  Lastly, &lt;svelte:options&gt; allows you to specify compiler options. We'll
-  use the immutable option as an example. In this app, the &lt;Todo&gt;
-  component flashes whenever it receives new data. Clicking on one of the items
-  toggles its done state by creating an updated todos array. This causes the
-  other &lt;Todo&gt; items to flash, even though they don't end up making any
-  changes to the DOM. We can optimise this by telling the &lt;Todo&gt; component
-  to expect immutable data. This means that we're promising never to mutate the
-  todo prop, but will instead create new todo objects whenever things change.
+  In all the examples we've seen so far, the &lt;script&gt; block contains code
+  that runs when each component instance is initialised. <br /> For the vast
+  majority of components, that's all you'll ever need. <br /> Very occasionally,
+  you'll need to run some code outside of an individual component instance.
+  <br />
+  For example, you can play all five of these audio players simultaneously; it
+  would be better if playing one stopped all the others. <br /> We can do that
+  by declaring a &lt;script context="module"&gt; block. <br /> Code contained
+  inside it will run once, when the module first evaluates, rather than when a
+  component is instantiated. <br />
 </p>
 
-The options that can be set here are:
-<ul>
-  <li>
-    immutable=&lbrace;true&rbrace; — you never use mutable data, so the compiler
-    can do simple referential equality checks to determine if values have
-    changed
-  </li>
-  <li>
-    immutable=&lbrace;false&rbrace; — the default. Svelte will be more
-    conservative about whether or not mutable objects have changed
-  </li>
-  <li>
-    accessors=&lbrace;true&rbrace; — adds getters and setters for the
-    component's props
-  </li>
-  <li>accessors=&lbrace;false&rbrace; — the default</li>
-  <li>
-    namespace="..." — the namespace where this component will be used, most
-    commonly "svg"
-  </li>
-  <li>
-    tag="..." — the name to use when compiling this component as a custom
-    element
-  </li>
-</ul>
+<!-- https://musopen.org/music/9862-the-blue-danube-op-314/ -->
+<AudioPlayer
+  src="https://sveltejs.github.io/assets/music/strauss.mp3"
+  title="The Blue Danube Waltz"
+  composer="Johann Strauss"
+  performer="European Archive"
+/>
+
+<!-- https://musopen.org/music/43775-the-planets-op-32/ -->
+<AudioPlayer
+  src="https://sveltejs.github.io/assets/music/holst.mp3"
+  title="Mars, the Bringer of War"
+  composer="Gustav Holst"
+  performer="USAF Heritage of America Band"
+/>
+
+<!-- https://musopen.org/music/8010-3-gymnopedies/ -->
+<AudioPlayer
+  src="https://sveltejs.github.io/assets/music/satie.mp3"
+  title="Gymnopédie no. 1"
+  composer="Erik Satie"
+  performer="Prodigal Procrastinator"
+/>
+
+<!-- https://musopen.org/music/2567-symphony-no-5-in-c-minor-op-67/ -->
+<AudioPlayer
+  src="https://sveltejs.github.io/assets/music/beethoven.mp3"
+  title="Symphony no. 5 in Cm, Op. 67 - I. Allegro con brio"
+  composer="Ludwig van Beethoven"
+  performer="European Archive"
+/>
+
+<!-- https://musopen.org/music/43683-requiem-in-d-minor-k-626/ -->
+<AudioPlayer
+  src="https://sveltejs.github.io/assets/music/mozart.mp3"
+  title="Requiem in D minor, K. 626 - III. Sequence - Lacrymosa"
+  composer="Wolfgang Amadeus Mozart"
+  performer="Markus Staab"
+/>
