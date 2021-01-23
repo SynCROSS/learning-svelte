3f36f12b491504a0715b3d96028379012059dcab
SynCROSS <51585533+yuto3125@users.noreply.github.com>
2021-01-23 15:28:01 +0900

    done:)

--- src/App.svelte
+++ src/App.svelte
@@ -1,18 +1,52 @@
 <script>
-  let user = {
-    firstname: 'Ada',
-    lastname: 'Lovelace',
-  };
+  import { onMount } from 'svelte';
+
+  let characters = ['🥳', '🎉', '✨'];
+
+  let confetti = new Array(100)
+    .fill()
+    .map((_, i) => {
+      return {
+        character: characters[i % characters.length],
+        x: Math.random() * 100,
+        y: -20 - Math.random() * 100,
+        r: 0.1 + Math.random() * 1,
+      };
+    })
+    .sort((a, b) => a.r - b.r);
+
+  onMount(() => {
+    let frame;
+
+    function loop() {
+      frame = requestAnimationFrame(loop);
+
+      confetti = confetti.map(emoji => {
+        emoji.y += 0.7 * emoji.r;
+        if (emoji.y > 120) emoji.y = -20;
+        return emoji;
+      });
+    }
+
+    loop();
+
+    return () => cancelAnimationFrame(frame);
+  });
 </script>
 
-<input bind:value={user.firstname} />
-<input bind:value={user.lastname} />
+{#each confetti as c}
+  <span style="left: {c.x}%; top: {c.y}%; transform: scale({c.r})"
+    >{c.character}</span
+  >
+{/each}
 
-{@debug user}
+<style>
+  :global(body) {
+    overflow: hidden;
+  }
 
-<h1>Hello {user.firstname}!</h1>
-
-<p>
-  If you want to pause execution, though, you can use the &lbrace;@debug
-  ...&rbrace; tag with a comma-separated list of values you want to inspect:
-</p>
+  span {
+    position: absolute;
+    font-size: 5vw;
+  }
+</style>
